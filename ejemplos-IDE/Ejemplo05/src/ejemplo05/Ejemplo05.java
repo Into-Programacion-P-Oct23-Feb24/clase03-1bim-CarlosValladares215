8fe3d33763a280aa3f1016b169f1b2e1aa548811
Carlos Valladares <csvalladares2@utpl.edu.ec>
2023-10-26 17:29:36 -0500

    Asignacion de booleanos

--- ejemplos-IDE/Ejemplo05/src/ejemplo05/Ejemplo05.java
+++ ejemplos-IDE/Ejemplo05/src/ejemplo05/Ejemplo05.java
@@ -18,26 +18,25 @@ public class Ejemplo05 {
         // TODO code application logic here
         System.out.println("----------------");
         System.out.println(10 / 3);
-        
+
         System.out.println("----------------");
         System.out.println(10 / 3.0);
-        
+
         System.out.println("----------------");
         System.out.println(10 / 3.0 + 1);
         System.out.println(10 / (3.0 + 1));
-        
+
         System.out.println("----------------");
-        System.out.println(10 % 3 );
-        
+        System.out.println(10 % 3);
+
         System.out.println("----------------");
-        System.out.println(Math.pow(10, 2));
-        
+        System.out.println(Math.pow(10, 2)); //el metodo pow me permite elevar a una potencia un valor dado
+
         System.out.println("----------------");
         System.out.println(Math.sqrt(16));
         
         double m = Math.sqrt(16);
         System.out.println(m);
-        
     }
 
 }
